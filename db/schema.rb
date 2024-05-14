5f017a12fc13e0bfce6e13f9b01834f1e0ef7213
Rodrigo Hoffmann <rhoffmannp@gmail.com>
2024-05-14 09:35:12 +0200

    added column to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_14_072656) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_14_073000) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
   end
 
 end
